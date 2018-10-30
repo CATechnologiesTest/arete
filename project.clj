34eafb9003f2cc8fadf99aad6f99071f429ac10d
Jerry R. Jackson <jrryjcksn@gmail.com>
2018-10-30 10:35:08 -0600

    Update project.clj to push to clojars

--- project.clj
+++ project.clj
@@ -1,14 +1,18 @@
-(defproject engine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject arete "0.6.0"
+  :description "Clojure rule engine"
+  :url "https://github.com/yipee.io/arete.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main ^:skip-aot engine.core
+  :main engine.viewer
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.flatland/ordered "1.5.6"]
                  [clj-yaml "0.4.0"]
                  [potemkin "0.4.5"]
                  [org.clojure/data.json "0.2.6"]
                  [org.javasimon/javasimon-core "4.1.3"]]
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :deploy-repositories [["releases"
+                         {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots"
+                         {:sign-releases false :url "https://clojars.org/repo"}]])
 
